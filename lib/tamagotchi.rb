c8ae8d0b78801b55f634a6c78e1bf1073f6798f1
Aubrey Masten and Sara Schultz <sara@>
2017-04-25 11:15:10 -0700

    pass first spec and initialize first methods

---
+++ lib/tamagotchi.rb
@@ -0,0 +1,24 @@
+class Tamagotchi
+  def initialize(name)
+    @name = name
+    @food_level = 10
+    @sleep_level = 10
+    @activity_level = 10
+  end
+
+  def name
+    @name
+  end
+
+  def food_level
+    @food_level
+  end
+
+  def sleep_level
+    @sleep_level
+  end
+
+  def activity_level
+    @activity_level
+  end
+end
---
+++ spec/tamagotchi_spec.rb
@@ -0,0 +1,15 @@
+require 'rspec'
+require 'tamagotchi'
+
+describe(Tamagotchi) do
+  describe '#initialize' do
+    it 'sets the name and life levels of a new Tamagotchi' do
+    my_pet = Tamagotchi.new('little dragon(the band)')
+    expect(my_pet.name).to(eq('little dragon(the band)'))
+    expect(my_pet.food_level).to(eq(10))
+    expect(my_pet.sleep_level).to(eq(10))
+    expect(my_pet.activity_level).to(eq(10))
+    end
+  end
+
+end

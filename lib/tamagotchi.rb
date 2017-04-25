1f3f527233ccb172c4de043469f6dd0c9858a7c2
Aubrey Masten and Sara Schultz <sara@>
2017-04-25 11:34:21 -0700

    pass tests for first four specs

--- lib/tamagotchi.rb
+++ lib/tamagotchi.rb
@@ -21,4 +21,16 @@ class Tamagotchi
   def activity_level
     @activity_level
   end
+
+  def is_alive
+    @food_level > 0 && @sleep_level > 0 && @activity_level > 0
+  end
+
+  def set_food_level (number)
+    @food_level = number
+  end
+
+  def time_passes
+    @food_level -= 1
+  end
 end
--- spec/tamagotchi_spec.rb
+++ spec/tamagotchi_spec.rb
@@ -12,4 +12,25 @@ describe(Tamagotchi) do
     end
   end
 
+  describe("#is_alive") do
+    it 'is alive if the food level is above 0' do
+      my_pet = Tamagotchi.new('little dragon(the band)')
+      expect(my_pet.is_alive).to(eq(true))
+    end
+
+    it 'is dead if the food level is 0' do
+      my_pet = Tamagotchi.new('little dragon(the band)')
+      my_pet.set_food_level(0)
+      expect(my_pet.is_alive).to(eq(false))
+    end
+  end
+
+  describe '#time_passes' do
+    it 'decreases the amount of food the Tamagotchi has left by 1' do
+      my_pet = Tamagotchi.new('little dragon(the band)')
+      my_pet.time_passes  #decide what trigger you will use to make time pass
+      expect(my_pet.food_level).to(eq(9))
+    end
+  end
+
 end
